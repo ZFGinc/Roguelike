2233202eda06c97a24375bb1f3f43367e6ff5554
ZFGinc <zfgcomp.01@gmail.com>
2025-03-16 21:44:55 +1100

    Initial commit

---
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FishNet.Connection;
+using FishNet.Object;
+
+public class PlayerController : NetworkBehaviour
+{
+    [Header("Base setup")]
+    [SerializeField] private float _walkingSpeed = 7.5f;
+    [SerializeField] private float _runningSpeed = 11.5f;
+    [SerializeField] private float _jumpSpeed = 8.0f;
+    [SerializeField] private float _gravity = 20.0f;
+    [SerializeField] private float _lookSpeed = 2.0f;
+    [SerializeField] private float _lookXLimit = 45.0f;
+    [SerializeField] private float _cameraYOffset = 0.4f;
+ 
+    private CharacterController characterController;
+    private Vector3 moveDirection = Vector3.zero;
+    private float rotationX = 0;
+    private bool _canMove = true;
+    private Camera _playerCamera;
+    private bool _isLoading = true; 
+    
+    public void OnSceneLoadEnd()
+    {
+        if (base.IsOwner)
+        {
+            _playerCamera = Camera.main;
+            _playerCamera.transform.position = new Vector3(transform.position.x, transform.position.y + _cameraYOffset, transform.position.z);
+            _playerCamera.transform.SetParent(transform);
+        }
+        else
+        {
+            gameObject.GetComponent<PlayerController>().enabled = false;
+        }
+        
+        _isLoading = false; 
+    }
+ 
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        _isLoading = true; 
+        NetworkSceneLoader.Instance.OnSceneLoaded += OnSceneLoadEnd;
+    }
+    
+    void Update()
+    {
+        if(_isLoading) return;
+        
+        bool isRunning = false;
+ 
+        // Press Left Shift to run
+        isRunning = Input.GetKey(KeyCode.LeftShift);
+ 
+        // We are grounded, so recalculate move direction based on axis
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+ 
+        float curSpeedX = _canMove ? (isRunning ? _runningSpeed : _walkingSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = _canMove ? (isRunning ? _runningSpeed : _walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+ 
+        if (Input.GetButton("Jump") && _canMove && characterController.isGrounded)
+        {
+            moveDirection.y = _jumpSpeed;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
+ 
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= _gravity * Time.deltaTime;
+        }
+ 
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+ 
+        // Player and Camera rotation
+        if (_canMove && _playerCamera != null)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * _lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -_lookXLimit, _lookXLimit);
+            _playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * _lookSpeed, 0);
+        }
+    }
+}
+ 
